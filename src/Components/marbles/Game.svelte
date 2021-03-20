bf37fabac2193d4fb467f65f235c182a414e32ba
Eric Grimsborn <eric.o.berglund@gmail.com>
2021-03-20 17:49:30 +0100

    show fireworks on game win

--- src/Components/marbles/Game.svelte
+++ src/Components/marbles/Game.svelte
@@ -2,6 +2,7 @@
     import { Game } from '../../marbles/Game';
     import { GameEvent } from '../../marbles/GameEvent';
     import type { RenderData } from '../../marbles/RenderData';
+    import Fireworks from './Fireworks.svelte';
     import Pipe from './Pipe.svelte';
 
     // TODO: Fireworks
@@ -10,19 +11,42 @@
 
     let game: Game | null = null;
     let renderData: RenderData | null = null;
+    let renderFireWorks: boolean = false;
+    let renderFireWorksTimer: number | null = null;
 
     const newGame = () => {
         game = new Game(6);
         renderData = game.getRenderData();
     };
 
+    const showFireWorks = () => {
+        renderFireWorks = true;
+        if (renderFireWorksTimer !== null) {
+            clearTimeout(renderFireWorksTimer);
+        }
+    };
+
+    const hideFireWorks = () => {
+        renderFireWorks = false;
+        if (renderFireWorksTimer !== null) {
+            clearTimeout(renderFireWorksTimer);
+        }
+    };
+
+    const flashFireWorks = (durationMs: number) => {
+        renderFireWorks = true;
+        renderFireWorksTimer = setTimeout(hideFireWorks, durationMs);
+    };
+
     const handleEvent = (event: GameEvent | null) => {
         switch (event) {
             case GameEvent.PipeFinished:
                 console.log('small firework');
+                flashFireWorks(1000);
                 break;
             case GameEvent.GameWon:
                 console.log('large firework');
+                showFireWorks();
                 break;
             default:
                 break;
@@ -45,6 +69,9 @@
             activePipeId={renderData.activePipe}
         />
     {/each}
+    {#if renderFireWorks}
+        <Fireworks />
+    {/if}
 </div>
 
 <style>
